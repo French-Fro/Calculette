1a18229534f4fc4cb366841d0bcae585399fcf38
Benjamin <benjamin.vitis@icloud.com>
2019-04-02 14:00:38 +0200

    Adding first test on multiplication test

--- src/test/java/MultiplicationTests.java
+++ src/test/java/MultiplicationTests.java
@@ -6,9 +6,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class MultiplicationTests{
+    Multiplication multi = new Multiplication();
+
     @Test
-    public void testMultiplication()  {
-        Multiplication multi = new Multiplication();
+    public void testMultiplication23()  {
         multi.setA(2);
         multi.setB(3);
         assertThat(multi.Multiplication(),equalTo(multi.getA()*multi.getB()));
