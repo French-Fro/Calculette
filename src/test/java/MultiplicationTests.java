01f2f3a22a75d1955dd1668a15252d40cca7b97c
Benjamin <benjamin.vitis@icloud.com>
2019-04-02 14:09:01 +0200

    Adding tests with negative value for both variables

--- src/test/java/MultiplicationTests.java
+++ src/test/java/MultiplicationTests.java
@@ -50,5 +50,11 @@ public class MultiplicationTests{
         multi.setB(0);
         assertThat(multi.Multiplication(),equalTo(multi.getA()*multi.getB()));
     }
+    @Test
+    public void testMultiplicationNegativBoth()  {
+        multi.setA(-1);
+        multi.setB(-1);
+        assertThat(multi.Multiplication(),equalTo(multi.getA()*multi.getB()));
+    }
     
 }
