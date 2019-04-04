82b01a77e07e9b66178a8ae2d1627db971e56cd1
Benjamin <benjamin.vitis@icloud.com>
2019-04-04 15:45:16 +0200

    Adding Calculatrice test class

---
+++ src/test/java/CalculatriceTests.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.*;
+import static org.junit.Assert.*;
+
+public class CalculatriceTests {
+
+    @Test
+    public void testTextFieldOne(){
+
+    }
+}
