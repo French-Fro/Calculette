6524b11b4ec18e4760104348b4a4edad231635eb
Benjamin <benjamin.vitis@icloud.com>
2019-04-04 15:54:28 +0200

    Adding first method test for calculatrice

--- src/test/java/CalculatriceTests.java
+++ src/test/java/CalculatriceTests.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.*;
 public class CalculatriceTests {
 
     @Test
-    public void testTextFieldOne(){
-
+    public void testAdditionOnTextFields(){
+        Calculatrice c = new Calculatrice();
+        // c.Addition(a,b);
+        assertThat(c.Addition(a,b), equalTo(1 - 0));
     }
 }
